70188f4275a9ea5a09f71ad3552e68e1519d07eb
Grosss <artemenko,nickolai@gmail.com>
2017-03-27 00:04:22 +0200

    Task2 is added

---
+++ Task2.Tests/BubbleSortTests.cs
@@ -0,0 +1,139 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2.Tests
+{
+    [TestFixture]
+    public class BubbleSortTests
+    {
+        [Test]
+        public void SortBySumAsc_PassedJaggedArray_ExpectedSortedBySumOfRowsArray()
+        {
+            int[][] actual = {
+                new int[] { 1, 2, 4, 5 },
+                new int[] { 7, 12, 31 },
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 8, 13, 1, 9 }
+            };
+            int[][] expected = {
+                new int[] { 1, 2, 4, 5 },
+                new int[] { 8, 13, 1, 9 },
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 7, 12, 31 }                
+            };
+
+            BubbleSort.SortBySumAsc(actual);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortByMaxAsc_PassedJaggedArray_ExpectedSortedByMaxElementsOfRowsArray()
+        {
+            int[][] actual = {
+                new int[] { 1, 2, 4, 5 },
+                new int[] { 7, 12, 31 },
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 8, 13, 1, 9 }
+            };
+            int[][] expected = {
+                new int[] { 1, 2, 4, 5 },
+                new int[] { 8, 13, 1, 9 },
+                new int[] { 7, 12, 31 },
+                new int[] { 35, 2, 2, 3, 2 }                
+            };
+
+            BubbleSort.SortByMaxAsc(actual);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortByMinAsc_PassedJaggedArray_ExpectedSortedByMinElementsOfRowsArray()
+        {
+            int[][] actual = {
+                new int[] { 1, 2, 4, 5 },
+                new int[] { 7, 12, 31 },
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 8, 13, 1, 9 }
+            };
+            int[][] expected = {
+                new int[] { 1, 2, 4, 5 },
+                new int[] { 8, 13, 1, 9 },
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 7, 12, 31 }
+            };
+
+            BubbleSort.SortByMinAsc(actual);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortBySumDesc_PassedJaggedArray_ExpectedSortedBySumOfRowsArray()
+        {
+            int[][] actual = {
+                new int[] { 1, 2, 4, 5 },
+                new int[] { 7, 12, 31 },
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 8, 13, 1, 9 }
+            };
+            int[][] expected = {
+                new int[] { 7, 12, 31 },
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 8, 13, 1, 9 },
+                new int[] { 1, 2, 4, 5 }           
+            };
+
+            BubbleSort.SortBySumDesc(actual);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortByMaxDesc_PassedJaggedArray_ExpectedSortedByMaxElementsOfRowsArray()
+        {
+            int[][] actual = {
+                new int[] { 1, 2, 4, 5 },
+                new int[] { 7, 12, 31 },
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 8, 13, 1, 9 }
+            };
+            int[][] expected = {
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 7, 12, 31 },
+                new int[] { 8, 13, 1, 9 },
+                new int[] { 1, 2, 4, 5 },         
+            };
+
+            BubbleSort.SortByMaxDesc(actual);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void SortByMinDesc_PassedJaggedArray_ExpectedSortedByMinElementsOfRowsArray()
+        {
+            int[][] actual = {
+                new int[] { 1, 2, 4, 5 },
+                new int[] { 7, 12, 31 },
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 8, 13, 1, 9 }
+            };
+            int[][] expected = {
+                new int[] { 7, 12, 31 },
+                new int[] { 35, 2, 2, 3, 2 },
+                new int[] { 1, 2, 4, 5 },
+                new int[] { 8, 13, 1, 9 }
+            };
+
+            BubbleSort.SortByMinDesc(actual);
+
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ Task2/BubbleSort.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class BubbleSort
+    {
+        public static void SortBySumAsc(int[][] jaggedArray)
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - i - 1; j++)
+                {
+                    if (jaggedArray[j].Sum() > jaggedArray[j + 1].Sum())
+                        Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                }
+            }
+        }
+
+        public static void SortByMaxAsc(int[][] jaggedArray)
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - i - 1; j++)
+                {
+                    if (jaggedArray[j].Max() > jaggedArray[j + 1].Max())
+                        Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                }
+            }
+        }
+
+        public static void SortByMinAsc(int[][] jaggedArray)
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - i - 1; j++)
+                {
+                    if (jaggedArray[j].Min() > jaggedArray[j + 1].Min())
+                        Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                }
+            }
+        }
+
+        public static void SortBySumDesc(int[][] jaggedArray)
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - i - 1; j++)
+                {
+                    if (jaggedArray[j].Sum() < jaggedArray[j + 1].Sum())
+                        Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                }
+            }
+        }
+
+        public static void SortByMaxDesc(int[][] jaggedArray)
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - i - 1; j++)
+                {
+                    if (jaggedArray[j].Max() < jaggedArray[j + 1].Max())
+                        Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                }
+            }
+        }
+
+        public static void SortByMinDesc(int[][] jaggedArray)
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                for (int j = 0; j < jaggedArray.Length - i - 1; j++)
+                {
+                    if (jaggedArray[j].Min() < jaggedArray[j + 1].Min())
+                        Swap(ref jaggedArray[j], ref jaggedArray[j + 1]);
+                }
+            }
+        }
+
+        #region Private methods
+        private static void Swap(ref int[] firstRow, ref int[] secondRow)
+        {
+            int[] temp = firstRow;
+            firstRow = secondRow;
+            secondRow = temp;
+        }
+        #endregion
+    }
+}
